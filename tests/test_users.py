d148823715563ada65704d06a563a2820d237f3d
hochy-domoy <suhininasasha@yandex.ru>
2026-04-21 16:56:40 +0300

    Маркировки pytest

--- tests/test_users.py
+++ tests/test_users.py
@@ -7,8 +7,11 @@ from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 # Импортируем функцию для проверки ответа создания юзера
 from tools.assertions.users import assert_create_user_response
+import pytest
 
 
+@pytest.mark.users
+@pytest.mark.regression
 def test_create_user():
     # Инициализируем API-клиент для работы с пользователями
     public_users_client = get_public_users_client()
